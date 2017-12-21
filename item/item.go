3ccdb0a84702116670297e4df6f3ce4129deef3b
heidimitre <heidi.mitre@gmail.com>
2017-12-21 11:52:50 -0700

    updated item attributes

--- item/item.go
+++ item/item.go
@@ -3,12 +3,14 @@ package item
 type Item struct {
 	Price       float64
 	Description string
+	Url			string
 }
 
-func CreateItem (newPrice float64, desc string)(item *Item){
+func CreateItem (newPrice float64, desc string, url string)(item *Item){
 	newItem := new(Item)
 	newItem.Price = newPrice
 	newItem.Description = desc
+	newItem.Url = url
 	return newItem
 }
 
--- main.go
+++ main.go
@@ -1,22 +1,30 @@
 package main
 
 import (
-	//"net/http"
-	//"io/ioutil"
 	"fmt"
+	"net/http"
+	"io/ioutil"
+	"regexp"
+	"strings"
+	"strconv"
 	"github.com/heidimitre/priceProject/item"
-	//"regexp"
-	//"os"
 )
 
 func main(){
-	//req,_ := http.NewRequest("GET", os.Getenv("TARGET"), nil)
-	//client:= http.Client{}
-	//response,_ := client.Do(req)
-	//body,_ := ioutil.ReadAll(response.Body)
-	//bodyString:= string (body)
-	//re := regexp.MustCompile("\"buyboxPrice\"\\:\"\\$([0-9]+).([0-9]+)")
-	item:= item.CreateItem(19.99, "Exploding Kittens")
+
+	tempURL:= ""
+
+
+	req,_ := http.NewRequest("GET", tempURL, nil)
+	client:= http.Client{}
+	response,_ := client.Do(req)
+	body,_ := ioutil.ReadAll(response.Body)
+	bodyString:= string (body)
+	re := regexp.MustCompile("\"buyboxPrice\"\\:\"\\$([0-9]+).([0-9]+)")
+	priceString:= re.FindAllString(bodyString, -1)[0]
+	priceString = priceString[strings.Index(priceString,"$")+1 :len(priceString)]
+	price,_ := strconv.ParseFloat(priceString, 64)
+
+	item:= item.CreateItem(price, "Exploding Kittens", tempURL)
 	fmt.Println(item.Description, item.Price)
-	//fmt.Println(re.FindAllString(bodyString, -1))
 }
